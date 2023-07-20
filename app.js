a3ff796de49487cea5ac380112fc6ceac2364de4
owell andry <81540116+owellandry@users.noreply.github.com>
2023-07-20 04:19:59 +0000

    Update files deletes

--- app.js
+++ app.js
@@ -2,25 +2,37 @@ const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
 const path = require('path');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 const PORT = 5000;
+
 // Configurar la carpeta 'public' como directorio de archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
+
 // Ruta para mostrar el contenido del archivo index.html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
 // Manejar la conexión de un nuevo usuario
 io.on('connection', (socket) => {
-  console.log('Usuario conectado');
-  // Manejar la desconexión del usuario
-  socket.on('disconnect', () => {
-    console.log('Usuario desconectado');
-  });
+    console.log('Usuario conectado');
+
+    // Escuchar el evento inputChange y emitir reloadPage
+    socket.on('inputChange', () => {
+        console.log('Usuario escribió algo');
+        io.emit('reloadPage'); // Notificar a todos los clientes que deben recargar la página
+    });
+
+    // Manejar la desconexión del usuario
+    socket.on('disconnect', () => {
+        console.log('Usuario desconectado');
+    });
 });
+
 // Iniciar el servidor en el puerto especificado
 server.listen(PORT, () => {
-  console.log(`Servidor web abierto en http://localhost:${PORT}`);
+    console.log(`Servidor web abierto en http://localhost:${PORT}`);
 });
---
+++ public/dist/conection.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+const cryptoJS = require('crypto-js');
+
+// Leer el archivo .psw y desencriptar las variables de entorno
+const pswFilePath = path.join(__dirname, '..', '.psw');
+const encryptedData = fs.readFileSync(pswFilePath, 'utf8');
+const decryptedData = cryptoJS.AES.decrypt(encryptedData, 'password-secreta').toString(cryptoJS.enc.Utf8);
+const envVariables = JSON.parse(decryptedData);
+
+// Establecer la conexión con la base de datos usando las variables de entorno
+const dbHost = envVariables.DB_HOST;
+const dbUser = envVariables.DB_USER;
+const dbPassword = envVariables.DB_PASSWORD;
+const dbDatabase = envVariables.DB_DATABASE;
